3c771da4bdabd3bac64b1e9441edf702931bdf42
Tai Doan <doantientai@gmail.com>
2020-06-24 13:01:15 +0200

    more tests

--- compute_dists_pair_DRIT.py
+++ compute_dists_pair_DRIT.py
@@ -11,8 +11,13 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 # parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/001_portrait/b2a/result_lpips.txt')
 # parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/002_edges2handbags/a2b')
 # parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/002_edges2handbags/a2b/result_lpips.txt')
-parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/002_edges2handbags/b2a')
-parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/002_edges2handbags/b2a/result_lpips.txt')
+# parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/002_edges2handbags/b2a')
+# parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/002_edges2handbags/b2a/result_lpips.txt')
+
+# parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/003_edges2shoes/a2b')
+# parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/003_edges2shoes/a2b/result_lpips.txt')
+parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/003_edges2shoes/b2a')
+parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/SOTAsDemos/DRIT/DRIT/outputs/003_edges2shoes/b2a/result_lpips.txt')
 
 
 parser.add_argument('--use_gpu', default=True, help='turn on flag to use GPU')
