c18121a6af8f9afb63571a20464b8275e583e2ff
Jupyter User <jupyter@cuda-mohamed.materiel.ibisc>
2020-09-18 14:43:06 +0200

    tests after ECCV

--- compute_dists_pair_InfoMUNIT.py
+++ compute_dists_pair_InfoMUNIT.py
@@ -14,12 +14,46 @@ parser.add_argument('--use_gpu', default=True, help='turn on flag to use GPU')
 # parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Models/ver_workshop/012_MUNIT_origin_cityscapes_64_cyc/tests/test_batch/b2a/b2a/result_lpips.txt')
 
 # 015_cityscapes_64_cyc
-parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Models/ver_workshop/015_cityscapes_64_cyc/tests/test_batch/a2b/a2b')
-parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Models/ver_workshop/015_cityscapes_64_cyc/tests/test_batch/a2b/a2b/result_lpips.txt')
+# parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Models/ver_workshop/015_cityscapes_64_cyc/tests/test_batch/a2b/a2b')
+# parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Models/ver_workshop/015_cityscapes_64_cyc/tests/test_batch/a2b/a2b/result_lpips.txt')
 # parser.add_argument('-d', '--dir', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Models/ver_workshop/015_cityscapes_64_cyc/tests/test_batch/b2a/b2a')
 # parser.add_argument('-o', '--out', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Models/ver_workshop/015_cityscapes_64_cyc/tests/test_batch/b2a/b2a/result_lpips.txt')
 
+# # 018_MUNIT_origin_dog2catDRIT_64
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/019_InfoMUNIT_dog2catDRIT_64/tests/test_batch_500k/b2a/b2a/')
+# parser.add_argument('-o', '--out', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/019_InfoMUNIT_dog2catDRIT_64/tests/test_batch_500k/b2a/b2a/result_lpips.txt')
+
+# test 200 images for edge-datasets ----------------------------
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/003_edge2bag_64/tests/test_batch_200i/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/003_edge2bag_64/tests/test_batch_200i/b2a/b2a/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/004_edge2shoe_64/tests/test_batch_200i/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/004_edge2shoe_64/tests/test_batch_200i/b2a/b2a/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/005_MUNIT_origin_edge2bag_64/tests/test_batch_200i/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/005_MUNIT_origin_edge2bag_64/tests/test_batch_200i/b2a/b2a/')
+
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/006_MUNIT_origin_edge2shoe_64/tests/test_batch_200i/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/006_MUNIT_origin_edge2shoe_64/tests/test_batch_200i/b2a/b2a/')
+
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/018_MUNIT_origin_dog2catDRIT_64/tests/test_batch_300k/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/018_MUNIT_origin_dog2catDRIT_64/tests/test_batch_300k/b2a/b2a/')
+
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/019_InfoMUNIT_dog2catDRIT_64/tests/test_batch_300k/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/019_InfoMUNIT_dog2catDRIT_64/tests/test_batch_300k/b2a/b2a/')
+
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/020_MUNIT_portrait_64/tests/test_batch_500k/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/020_MUNIT_portrait_64/tests/test_batch_500k/b2a/b2a/')
+
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/022_InfoMUNIT_portrait_64/tests/test_batch_500k/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/022_InfoMUNIT_portrait_64/tests/test_batch_500k/b2a/b2a/')
+
+### Experiments after ECCV before WACV
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/024_InfoMUNIT_infoLen_4/tests/test_batch_800k/a2b/a2b/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/024_InfoMUNIT_infoLen_4/tests/test_batch_800k/b2a/b2a/')
+# parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/026_InfoMUNIT_infoLen_8/tests/test_batch_800k/a2b/a2b/')
+parser.add_argument('-d', '--dir', type=str, default='/home/jupyter/workdir/TaiDoan/Projects/InfoMUNIT_workshop/Models/026_InfoMUNIT_infoLen_8/tests/test_batch_800k/b2a/b2a/')
+
 opt = parser.parse_args()
+opt.out = os.path.join(opt.dir, 'result_lpips.txt')
 
 ## Initializing the model
 model = models.PerceptualLoss(model='net-lin', net='alex', use_gpu=opt.use_gpu)
